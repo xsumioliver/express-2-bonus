44c54f8836cdbe9bd4db035bae28011b5f34abb1
Xsumi Oliver <32101447+xsumioliver@users.noreply.github.com>
2017-11-20 10:22:19 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,63 @@
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+var rocks = [
+    {"user":"rock","ai":"rock","result":"tie"},
+    {"user":"rock","ai":"paper","result":"lose"},
+    {"user":"rock","ai":"scissors","result":"win"}
+  ]
+
+var papers =[
+    {"user":"paper","ai":"rock","result":"win"},
+    {"user":"paper","ai":"paper","result":"tie"},
+    {"user":"paper","ai":"scissors","result":"lose"}
+]  
+
+var scissorss =[
+{"user":"scissors","ai":"rock","result":"lose"},
+{"user":"scissors","ai":"paper","result":"win"},
+{"user":"scissors","ai":"scissors","result":"tie"}
+]
+
+const express = require('express') // import express
+const app = express() // create an express server
 const morgan = require('morgan')
-const express = require('express')
-const bodyParser = require("body-parser")
-var serveStatic = require('serve-static')
-const port = 8000;
-
-
-const app = express()
+const port = 3000; // we will use this later
 
 app.use(morgan('dev'))
-app.use(serveStatic("files"))
-app.use(bodyParser())
 
-app.get('/:op/:num1/:num2', (req, res) => {
-    if (!["add", "sub", "mul", "div"].includes(req.params.op)){
-      res.status(404)
-      res.send("please use add, sub, mul, or div")
-    }
-    const num1 = Number(req.params.num1)
-    const num2 = Number(req.params.num2)
-    res.send({
-      number1:num1,
-      number2: num2,
-      result: {
-        add: () => num1 + num2,
-        sub: () => num1 - num2, 
-        mul: () => num1 * num2, 
-        div: () => num1 / num2 
-      }[req.params.op]()
-    })
+app.get('/', (req, res) => {
+    res.send('<h1>Rock, Paper, Scissors, Shoot!</h1> <a href ="rock">Rock</a> <a href ="scissors">Scissors</a> <a href ="paper">Paper</a>')
+  })
   
-})
+app.get('/rock', (req, res) => {
+    res.send(shuffle(rocks), '<a href ="/">home</a>')
+  }) 
 
-app.post("/home.html", (req,res) => {
-  res.send(`hi, ${req.body.name}`)
-})
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.get('/paper', (req, res) => {
+    res.send(shuffle(papers), ' <a href ="/">home</a>')
+  }) 
+
+app.get('/scissors', (req, res) => {
+    res.send(shuffle(scissorss), ' <a href ="/">home</a>')
+  }) 
+
+  app.get('*', (req, res) => {
+    res.redirect('/')
   })
